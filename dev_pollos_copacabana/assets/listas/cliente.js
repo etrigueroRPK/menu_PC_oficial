cbed27950d2e220fcd10e11e9be5e3f4ec7e9c18
Edmundo_linux <edmundotriguero@gmail.com>
2019-11-27 16:42:40 -0400

    4

---
+++ dev_pollos_copacabana/assets/listas/cliente.js
@@ -0,0 +1,4 @@
+var img_cliente = new Array(
+    'assets/img/publicidad/RPK PLACA.jpg',
+    'assets/img/publicidad/YPFB PLACA.jpg'
+); 
---
+++ dev_pollos_copacabana/assets/listas/pollos.js
@@ -0,0 +1,18 @@
+var img_pollos = new Array(
+    'assets/img/publicidad/1.jpg',
+    'assets/img/publicidad/2.jpg',
+    'assets/img/publicidad/3.jpg',
+    'assets/img/publicidad/4.jpg',
+    'assets/img/publicidad/5.jpg',
+    'assets/img/publicidad/6.jpg',
+    'assets/img/publicidad/7.jpg',
+    'assets/img/publicidad/8.jpg',
+    'assets/img/publicidad/9.jpg',
+    'assets/img/publicidad/10.jpg',
+    'assets/img/publicidad/11.jpg',
+    'assets/img/publicidad/12.jpg',
+    'assets/img/publicidad/13.jpg',
+    'assets/img/publicidad/14.jpg',
+    'assets/img/publicidad/15.jpg',
+	'assets/img/publicidad/16.jpg'
+); 
